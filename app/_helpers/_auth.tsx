18164e1ec022534334138a4217f548a0df245d29
leandro <leandrocamposperes@gmail.com>
2024-03-14 20:43:05 -0300

    fix: next env url

--- app/_helpers/_auth.tsx
+++ app/_helpers/_auth.tsx
@@ -1,4 +1,3 @@
-"use server";
 import { cookies } from "next/headers";
 import { createClient } from "../utils/supabase/server";
 import { redirect } from "next/navigation";
@@ -12,16 +11,4 @@ export async function getUser() {
   }
 
   return data.user;
-}
-
-export function getURL(): string {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000/";
-  // Make sure to include `https://` when not localhost.
-  url = url.includes("http") ? url : `https://${url}`;
-  // Make sure to include a trailing `/`.
-  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-  return url;
 }
