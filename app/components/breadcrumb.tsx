4586f0780173ca32627a237e0240241db6a4ac00
leandro <leandrocamposperes@gmail.com>
2024-04-22 19:56:24 -0300

    fix(breadcrumb): client html error

--- app/components/breadcrumb.tsx
+++ app/components/breadcrumb.tsx
@@ -34,20 +34,5 @@ export default function Breadcrumb2() {
         })}
       </BreadcrumbList>
     </Breadcrumb>
-
-    // <div className="w-full border-b-2 pb-2 text-xs">
-    //   {pathNames.map((path, index) => {
-    //     return (
-    //       <span key={path} className={"pr-2 text-black"}>
-    //         {pathNames.length === index + 1 ? (
-    //           <span className="text-red-500"> {path.toUpperCase()} </span>
-    //         ) : (
-    //           <span> {path.toUpperCase()} </span>
-    //         )}
-    //         /
-    //       </span>
-    //     );
-    //   })}
-    // </div>
   );
 }
